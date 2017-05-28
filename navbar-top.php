8b17c6766a1d03675a53be3d76129077492e14af
TropicalBananaMan <mrmineturtle15@gmail.com>
2017-05-28 16:15:06 +0200

    fikset hovedside og om-oss

---
+++ navbar-top.php
@@ -0,0 +1,10 @@
+<!--Navbar øverst som har links i følgende rekkefølge: Hjem, About, (LOGO), Contact Us og Campus.
+    Det skal også være en link til login. -->
+  <div id="navigation-top">
+      <a href="index.php">Hjem</a>
+      <a href="om_oss.php">Om Oss</a>
+      <img id="logo" src="images/logo-top.png" alt="">
+      <a href="#">Ta Kontakt</a>
+      <a href="#">Login/Registrer</a>
+      <hr>
+  </div>
---
+++ om_oss.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/om_oss_style.css">
+    <link rel="stylesheet" href="css/navbar_top.css">
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
+    <title>Om Oss</title>
+  </head>
+  <body>
+    <!--Importer navbar-top her. -->
+    <?php require 'navbar-top.php' ?>
+
+    <div id="content-container">
+      <h1>Hvem er vi?</h1>
+      <p>Velkommen til Kork.
+          Kork er en oppslagstavle skreddersydd for Westerdals ACT.
+          Her kan man spørre om leksehjelp, finne et nytt bokollektiv, rabattkuponger på et spisested i nærheten, invitere til fest, få tilbake noe som er mistet, eller kanskje finne kjærligheten - akkurat som en god gammeldags korktavle på nærbutikken.
+
+          På siden kan man navigere seg mellom campusene eller bare få en oversikt over alle stedene.
+          Hva skal du korke?</p>
+    </div>
+
+     <div class="footer"><p>Copyright (c) 2017 Westerdals Kork Co. All Rights Reserved.</p></div>
+
+  </body>
+</html>
